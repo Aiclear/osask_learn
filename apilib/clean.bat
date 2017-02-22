0bc2e5e4eb223e8cf2b5dd8e462f4d15f312ed0e
Aiclear <xinzheyyu@gmail.com>
2017-02-22 19:18:18 +0800

    end of 27 osask

---
+++ apilib/clean.bat
@@ -0,0 +1,3 @@
+@echo off
+del *.lst *.obj *.hrb *.bim *.map
+del apilib.lib
--- clean.bat
+++ clean.bat
@@ -1,4 +1,5 @@
 @echo off
 del *.bin *.lst *.gas *.obj *.hrb *.bim *.map
 del bootpack.nas bootpack.map bootpack.bim bootpack.hrb haribote.sys
+del haribote.img
 
